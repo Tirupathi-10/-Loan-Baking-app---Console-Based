86bac111a49cbc0efbfd7800de8439a629dd312b
Tirupathi-10 <baditirupathi1@gmail.com>
2026-09-10 21:27:36 +0530

    Loan Banking Application(POC) Console Based

---
+++ javaproject1/src/com/Oops/Loan.java
@@ -0,0 +1,22 @@
+package com.Oops;
+
+interface Loan {
+
+	String getAddressDetails();
+
+	boolean isPhoneValid();
+
+	boolean isAadharValid();
+
+	boolean isPANValid();
+
+	double getCustomerSalary();
+
+	int getCibiScore();
+
+	int ageInfo();
+
+	double getROI();
+	
+
+}

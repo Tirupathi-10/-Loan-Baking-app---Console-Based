8f657e84eccdaaca1ca3b8c2336c9c3870a6e03d
Tirupathi-10 <baditirupathi1@gmail.com>
2026-09-10 12:46:23 +0530

    Banking Loan Application

---
+++ javaproject1/src/com/Oops/Inheritance/Loan.java
@@ -0,0 +1,22 @@
+package com.Oops.Inheritance;
+
+interface Loan {
+
+	String getAddressDetails();
+
+	boolean isPhoneValid();
+
+	boolean isAadharValid();
+
+	boolean isPANValid();
+
+	double getCustomerSalary();
+
+	int getCibiScore();
+
+	int ageInfo();
+
+	double getROI();
+	
+
+}
